1da81f8335948391723202197a73e11c5a27c744
Helen <elenaprofessor@yandex.ru>
2022-08-21 15:39:01 +0300

    Задача 6 исправленная

---
+++ Папка/папка2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Номер 6 Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным.
+Console.WriteLine ("Введите любое число..");
+int i = int.Parse(Console.ReadLine());
+if (i%2==0)
+{
+    Console.WriteLine ("Число четное");
+}
+else 
+{
+    Console.WriteLine ("Число нечетное");
+}
