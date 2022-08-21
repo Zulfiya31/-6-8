92a6a968c3f0b6753ca3c75094139ee37dcf0cf1
Helen <elenaprofessor@yandex.ru>
2022-08-21 15:03:39 +0300

    Change

---
+++ Папка/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 8 на нахождение четного из числа N
+Console.WriteLine ("Введите любое число..");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(n);
+int i = 1;
+while (i < n)
+{
+    if(i % 2 == 0)
+    {
+        Console.WriteLine(i);
+    }
+    i++;
+}
+   
