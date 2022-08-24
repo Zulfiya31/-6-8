1140e9bd3ac25bd2e4d516e58c05f03cf83780c7
Zulfiya31 <110923638+Zulfiya31@users.noreply.github.com>
2022-08-24 11:48:18 +0300

    Update Program.cs

--- задания/задача 13/Program.cs
+++ задания/задача 13/Program.cs
@@ -5,9 +5,10 @@ if(num<99)
     Console.WriteLine("Третьей цифры нет");
 }   
 if(num>99){
-    Console.WriteLine ("Неразобрал, что за число вы ввели, введите снова ");
+    Console.WriteLine ("Не разобрал, что за число вы ввели, введите снова ");
     string str = Console.ReadLine ();
     int n = str.Length;
     Console.WriteLine  ("Все теперь увидел, вот ваша третья цифра");
     Console.WriteLine  (str[2]);
 }
+// Могу сделать без дополнительного ввода если скажете, но мне так больше нравится :)
